600a681afbfa6e364ee824b42bb95a8a3dc78a20
worksofallwin <princeallwin199@gmail.com>
2024-01-19 00:02:57 +0530

    complete contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import MediaLinks from "@/components/Home/MediaLinks";
 
 const Contact = () => {
-    return <div></div>;
+    return (
+        <div className="py-16">
+            <div className="mt-24 max-w-[1000px] mx-auto">
+                <p className="uppercase text-xl text-primary mb-6">Contact</p>
+                <p className="text-4xl">Prince Allwin</p>
+                <p className="text-xl mb-8">Smart Contract Engineer</p>
+                <p className="text-base">
+                    I&apos;m open to both full-time and remote opportunities.
+                </p>
+                <p>Contact me and let&apos;s talk.</p>
+                <p className="py-2 text-primary mt-8">
+                    princeallwin199@gmail.com
+                </p>
+                <div className="mt-4">
+                    <MediaLinks />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
