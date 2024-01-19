dc44fc87157deccedcc6ff43baffa9a8bb6a8886
worksofallwin <princeallwin199@gmail.com>
2024-01-19 23:38:11 +0530

    complete contributions page structure

--- src/app/contributions/page.tsx
+++ src/app/contributions/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import { MdArrowRight } from "react-icons/md";
 
 const Contributions = () => {
     return (
@@ -7,7 +9,54 @@ const Contributions = () => {
                 <p className="uppercase text-xl text-primary mb-6">
                     Contributions
                 </p>
-                <div></div>
+                <div className="mt-10">
+                    <p className="text-2xl">Openzeppelin</p>
+                    <div>
+                        <p className="mt-6">
+                            Add internal functions inside modifiers
+                        </p>
+                        <p className="mt-1 text-sm">
+                            Id :{" "}
+                            <Link
+                                href="https://github.com/OpenZeppelin/openzeppelin-contracts/pull/4472"
+                                className="text-sm text-primary"
+                                target="_blank"
+                            >
+                                #4472
+                            </Link>
+                        </p>
+                        <div className="mt-4 text-sm text-gray-600">
+                            <ul className="m-0">
+                                <li className="py-1 flex">
+                                    <MdArrowRight
+                                        size={20}
+                                        className="text-gray-600"
+                                    />{" "}
+                                    The integration of internal functions within
+                                    modifiers contributes to improved
+                                    readability.
+                                </li>
+                                <li className="py-1 flex">
+                                    <MdArrowRight
+                                        size={20}
+                                        className="text-gray-600"
+                                    />{" "}
+                                    This approach not only reduces gas
+                                    consumption but also improves
+                                    maintainability.
+                                </li>
+                                <li className="py-1 flex">
+                                    <MdArrowRight
+                                        size={20}
+                                        className="text-gray-600"
+                                    />{" "}
+                                    Furthermore, it enhances the overall
+                                    structure and organization of your code.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
